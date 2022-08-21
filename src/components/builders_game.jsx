31b17c1e5f4cdd4265758e75cad49b9b5bc203d8
BrandonLenz <lenzb@oregonstate.edu>
2022-08-21 13:38:46 -0400

    Committing with now react errors

---
+++ src/components/builders_game.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class BuildersGame extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <h1>The builder's game is under construction still.</h1>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default BuildersGame;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,16 +3,20 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     state = {  } 
     render() { 
+
+        const aboutMeURL = "";
+        const buildersURL = "";
+
         return (
             <React.Fragment>
                 <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link text-white">About Me</a>
+                            <a className="nav-link text-white" href={aboutMeURL}>About Me</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white">Builders Game</a>
+                            <a className="nav-link text-white" href={buildersURL}>Builders Game</a>
                         </li>
                         </ul>
                     </div>
