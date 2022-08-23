48b44fccf5f3b4fe3946b366fe012a0edef3e24e
BrandonLenz <lenzb@oregonstate.edu>
2022-08-22 21:49:13 -0400

    cleaned up the links

--- src/components/about_me.jsx
+++ src/components/about_me.jsx
@@ -4,6 +4,7 @@ import OSULogo from '../images/osulogo.svg'
 class AboutMe extends Component {
 
     render() { 
+
         return (
             <div className="container">
                 <div className="row">
@@ -19,18 +20,26 @@ class AboutMe extends Component {
                             </div>
                             
                             <div className="col"align="center">
-                                <h5><a href="https://oregonstate.edu/" target="_blank" rel="noreferrer">Oregon State University</a></h5>
+                                <h4>
+                                    <a href="https://oregonstate.edu/" target="_blank" rel="noreferrer" className="text-decoration-none">
+                                        Oregon State University
+                                    </a>
+                                </h4>
                                 <h6>B.S. in Computer Science</h6>
                                 <h6>September 2020 - Expected Spring 2023</h6>
                             </div>
                             <div className="col" />
-                            <div className="w-100 m-2"></div>
+                            <div className="w-100 m-4"></div>
 
                             <div className="col">
                                 <img src={require('../images/uwmadison.0fd53df6.png')} alt="University of Wisconsin Madison Logo" width={400}/>
                             </div>
                             <div className="col" align="center">
-                                <h5><a href="https://www.wisc.edu/" target="_blank" rel="noreferrer">University of Wisconsin, Madison</a></h5>
+                                <h4>
+                                    <a href="https://www.wisc.edu/" target="_blank" rel="noreferrer" className="text-decoration-none">
+                                        University of Wisconsin, Madison    
+                                    </a>
+                                </h4>
                                 <h6>B.S. in Nuclear Engineering with minors in Mathematics and German Language</h6>
                                 <h6>September 2010 - May 2015</h6>
                             </div>
