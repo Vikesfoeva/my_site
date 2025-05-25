ea0cd026e026657ca6812b561b1c571d9cb466ea
Brandon <Vikingblenz@gmail.com>
2025-05-25 00:40:29 -0400

    chore: remove unused files and components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './css/App.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/bootstrapOverrides.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
